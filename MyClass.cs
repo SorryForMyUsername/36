33ebc3752e91b51c501d9ddf7f8d8560d1652bea
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-04-11 12:16:26 +0300

    Add files via upload

---
+++ MyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _36
+{
+    internal class MyClass : IMyInterface
+    {
+        public void Show()
+        {
+            Console.WriteLine("MyClass.Show() is implemented (реализован).");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _36
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Изучение интерфейсов. Пример - 1***\n");
+            MyClass myClass0b = new MyClass();
+            myClass0b.Show();
+            Console.Read();
+        }
+    }
+}

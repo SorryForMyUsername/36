bf31ec44be6d76e6b3c7e76295db20e013ebd4a0
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-04-11 12:27:58 +0300

    Add files via upload

---
+++ MyClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _36
+{
+    internal class MyClass : IMyInterfaceA, IMyInterfaceB
+    {
+        public void Show()
+        {
+            Console.WriteLine("MyClass.Show() is completed");
+        }
+
+        void IMyInterfaceA.Show()
+        {
+            Console.WriteLine("Explicit interface Implementation. IMyInterfaceA.Show().");
+        }
+        void IMyInterfaceB.Show()
+        {
+            Console.WriteLine("Explicit interface Implementation. IMyInterfaceB.Show().");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _36
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Изучение интерфейсов. Пример - 4***\n");
+            MyClass myClassOb = new MyClass();
+            myClassOb.Show();
+            IMyInterfaceA interA = myClassOb;
+            interA.Show();
+            IMyInterfaceB interB = myClassOb;
+            interB.Show();
+            Console.Read();
+        }
+    }
+}

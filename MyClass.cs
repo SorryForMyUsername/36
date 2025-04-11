46eb8099fe9339fb4d6ff64628bdeaf928aa9e0d
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-04-11 12:23:12 +0300

    Add files via upload

---
+++ MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _36
+{
+    internal class MyClass : IMyInterfaceA, IMyInterfaceB
+    {
+        public void ShowA()
+        {
+            Console.WriteLine("MyClass.ShowA() is completed.");
+        }
+        public void ShowB()
+        {
+            Console.WriteLine("MyClass.ShowB() is completed.");
+        }
+    }
+}

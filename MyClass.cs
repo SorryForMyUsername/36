d4a981f8c15e992c9af0d579262b4110326d9a23
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-04-11 12:38:17 +0300

    Add files via upload

---
+++ MyClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _36
+{
+    internal class MyClass : InterfaceC
+    {
+        public void ShowInterfaceA()
+        {
+            Console.WriteLine("ShowInterfaceA() is completed");
+        }
+        public void ShowInterfaceB()
+        {
+            Console.WriteLine("ShowInterfaceB() is completed");
+        }
+        public void ShowInterfaceC()
+        {
+            Console.WriteLine("ShowInterfaceC() is completed");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _36
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Изучение интерфейсов. Пример - 5***\n");
+            Console.WriteLine("***Concept of multiple inheritance through interface***\n");
+            MyClass myClassOb = new MyClass();
+            InterfaceA obA = myClassOb;
+            obA.ShowInterfaceA();
+            InterfaceB obB = myClassOb;
+            obB.ShowInterfaceB();
+            InterfaceC obC = myClassOb;
+            obC.ShowInterfaceC();
+            Console.Read();
+        }
+    }
+}
